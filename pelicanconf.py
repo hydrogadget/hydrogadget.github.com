f29cadc1f4dbec62b4df59e418afd1fbfb69ee38
Mike Walker <noisufnoc@gmail.com>
2012-11-30 21:44:51 -0800

    more cleanup before first post

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ AUTHOR = u"HydroGadget"
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 SITENAME = u"HydroGadget"
-SITEURL = ''
+SITEURL = 'http://hydrogadget.org'
 
 GITHUB_URL = 'https://github.com/hydrogadget/'
 TWITTER_USERNAME = 'hydrogadget'
@@ -18,8 +18,8 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Blogroll
 LINKS =  (
-	 ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
          ('RaspberryPi.org', 'http://raspberrypi.org'),
+	 ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
          ('Python.org', 'http://python.org'),
 )
 
@@ -30,5 +30,8 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = 10
 
-LOGOTEXT = 'HydroGadget'
-LOGOIMAGE = '/theme/images/hydrogadget.png'
+# I dont think this works the way I thought it would.
+# Sadface.
+
+#LOGOTEXT = 'HydroGadget'
+#LOGOIMAGE = '/theme/images/hydrogadget.png'
